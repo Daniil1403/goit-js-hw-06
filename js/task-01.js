8b1622717ed85044d2cde1b558f7563413ab3df7
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-09-01 07:26:56 +0300

    add task 1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const navLiEl = document.querySelectorAll('.item');
+console.log('Number of categories:', navLiEl.length);
+
+const titleEl = document.querySelectorAll('.item h2');
+console.log('Category:', titleEl[0].textContent);
+
+const liEl = document.querySelectorAll('.item li');
+console.log('Elements:', liEl.length - 8);
+
+console.log('Category:', titleEl[1].textContent);
+console.log('Elements:', liEl.length - 9);
+
+console.log('Category:', titleEl[2].textContent);
+console.log('Elements:', liEl.length - 7);
