9c829f0619ee14b3fa991adeb3700793cc834bc0
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-09-03 10:21:45 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+ 'Potatoes',
+ 'Mushrooms',
+ 'Garlic',
+ 'Tomatos',
+ 'Herbs',
+ 'Condiments',
 ];
+
+const createLi = document.querySelector('.ingredients');
+
+const elements = [];
+
+//for (let i = 0, i < ingredients.length, i += 1) {
+//const option = ingredients[i];
+
+//const elementLi = document.createElement('li');
+//const elementLi.classList.add('item');
+//const elementLi.textContent = option;
+
+//elements.push(elementLi);
+//}
+
+//console.log(elements);
+
+//createLi.append[...elements];
+
+//const ingredientsRef = document.querySelector('li');
+//const ingredientsRef.classList.add('item');
+//const ingredientsRef.textContent = option;
