ac9c89d5f238f54f037a70941fc14688ec99126b
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-09-18 18:06:02 +0300

    add 10 task

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const nameSpan = document.querySelector('#name-output');
+const input = document.querySelector('#name-input');
+
+const onInputPrinting = (event) => {
+ nameSpan.textContent = event.currentTarget.value;
+ event.currentTarget.value.trim() === ''
+  ? (nameSpan.textContent = 'Anonymous')
+  : null;
+};
+input.addEventListener('input', onInputPrinting);
