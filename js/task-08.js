922323418757bc95d49ac154999bad2d356f0de4
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-09-18 10:57:09 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,11 @@
-
+document.querySelectorAll(`.login-form`);
+login - form.querySelector(`submit`, OnSubmit);
+function OnSubmit(evt) {
+ evt.preventDefault();
+ console.dir(evt.currentTaeget);
+ const { email, password } = evt.currentTaeget;
+ const data = {
+  email: email.value,
+  password: password.value,
+ };
+}
